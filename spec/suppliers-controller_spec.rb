1d9d8a366bb5edcb504c6d5750a999d4828812f2
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2025-06-15 22:09:26 -0300

    add test for get all suppliers

--- spec/suppliers-controller_spec.rb
+++ spec/suppliers-controller_spec.rb
@@ -24,4 +24,18 @@ describe SupplierController do
     expect(result[:status]).to eq(404)
     expect(result[:data]).to eq('Supplier not found!')
   end
+
+  it 'gets all suppliers from db' do
+    result = controller.get_all_suppliers
+    expect(result).to have_key(:data)
+    expect(result[:data]).to be_truthy
+    expect(result[:data].length).to eq(4)
+  end
+
+  it 'gets a supplier passing a string as query parameter' do
+    result = controller.get_all_suppliers(name: '2')
+    expect(result).to have_key(:data)
+    expect(result[:data]).to be_truthy
+    expect(result[:data]).to include(have_attributes(name: 'supplier 02'))
+  end
 end
